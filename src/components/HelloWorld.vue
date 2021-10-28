537fc2920e415b04a9cce24ec61afacbf82723f7
Haniel Hernandez <hhernandez@aimservices.tech>
2021-10-27 19:39:14 -0600

    Lint and Tailwind installed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -119,14 +119,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: String
   },
-});
+  setup() {
+    const t = 't'
+    console.log(t)
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
